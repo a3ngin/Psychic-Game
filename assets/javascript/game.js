8425018d49689f1485f67c91e5ea67f64066b9eb
Anne Engin <anne@Annes-MacBook-Air.local>
2019-02-28 23:02:49 -0500

    functioning sortof

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,25 +8,36 @@
     var win = 0;
     var loss = 0;
     var triesleft = 9;
-    
+    var letter = any;
 
     document.onkeyup = function(event) {
         var userGuess = event.key.toLowerCase();
-    }
-
-    
-        for (triesleft = 9; triesleft > 0; triesleft --){
-            if (userGuess === letter) {
+        console.log(userGuess);
+        console.log(letter);
+            if (userGuess == letter) {
                 win++;
-                triesleft = 0;}
-            if (userGuess!= letter) {triesleft--;}
+                reset();
+                }
+            if (userGuess != letter) {
+                triesleft--;
+                if(triesleft == 0) {loss++; 
+                reset();
+            }
+            
         }
-
+        console.log(win);
+        console.log(triesleft);
+        console.log(loss); 
 
     }
 
 
 
+    function reset(){
+        triesleft = 9;
+        randomletter();
+        console.log(letter);
+    }
 
 
 
@@ -37,6 +48,7 @@
         console.log(x);
         var letter = str.charAt(x);
         console.log(letter);
+        return letter;
     }
     
     
